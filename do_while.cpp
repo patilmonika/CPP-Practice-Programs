7fae606eb4030151970a674168afbda0ffc93fed
patilmonika <50772846+patilmonika@users.noreply.github.com>
2021-11-25 13:44:56 +0530

    Add files via upload
    Example of "do_while " loop

---
+++ do_while.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, i;
+    n = 20;
+    i = 2;
+    // 3, 6, 9.......30
+    // 2, 4, 6, 8, ...20
+    //cout<<n;
+    do {
+       // int x = n * i;
+        cout<<", ";
+        cout<<n * i;
+        i++;
+       
+    }
+    while (i>=5);
+    
+    return 0;
+}
