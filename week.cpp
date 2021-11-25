0695bcfb1bb7f4dd3f078c09cc6ff32db67b6bc8
patilmonika <50772846+patilmonika@users.noreply.github.com>
2021-11-25 13:46:09 +0530

    Add files via upload

---
+++ week.cpp
@@ -0,0 +1,25 @@
+
+#include<iostream>
+using namespace std;
+int main()
+{
+int week;
+cout<<"Enter the number of the day"<<endl;
+cin>>week;
+if (week==1)
+cout<<"Monday";
+else if (week==2)
+cout<<"Tuesday";
+else if (week==3)
+cout<<"Wednesday";
+else if(week==4)
+cout<<"Thursday";
+else if(week==5)
+cout<<"Friday";
+else if(week==6)
+cout<<"Saturday";
+else if(week==7)
+cout<<"Sunday";
+else
+cout<<"The number is invalid";
+}
